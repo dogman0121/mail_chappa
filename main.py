d0dcfc93c0b679487967f781ad29d6ddb654b9c0
dogman0121 <giachetti101@gmail.com>
2024-12-24 00:52:25 +0300

    Add .env support and html parsing

--- main.py
+++ main.py
@@ -2,32 +2,41 @@ import json
 
 from inbox import Inbox
 from requests import post
+from dotenv import load_dotenv
 
 import os
 
 
+load_dotenv()
+
 inbox = Inbox()
+
+
+def parse_http_response(response: bytes):
+    if b"\r\n\r\n" in response:
+        headers, body = response.split(b"\r\n\r\n")
+    elif b"\n\n" in response:
+        headers, body = response.split(b"\n\n")
+    else:
+        raise ValueError("Unknown separator")
+
+    return headers, body
 
 
 @inbox.collate
 def handle(to, sender, subject, body):
-    print(to)
-    print("--------")
-    print(sender)
-    print("--------")
-    print(subject)
-    print("--------")
-    print(body)
+    html = parse_http_response(body)[1]
     if os.environ.get("REDIRECT_URL"):
         post(
             "REDIRECT_URL",
             data=json.dumps({
                 "to": to,
                 "sender": sender,
-                "body": body
+                "text": subject,
+                "html": html
             }))
 
 
 if __name__ == "__main__":
-    inbox.serve(25,"217.144.189.150")
+    inbox.serve(os.environ.get("PORT"), os.environ.get("ADDRESS"))
     inbox.dispatch()
